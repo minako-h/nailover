3dd39da4a167171ce030266df1f97f0c7e6bf304
minako-h <hawaii77777m@icloud.com>
2020-05-01 10:53:01 +0900

    fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ApplicationRecord
+  validates :name, :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_04_30_082312) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
+    t.integer "movie_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
