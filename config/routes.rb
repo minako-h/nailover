d0cc2463c2279fc9b90d9956e5214803c71f9b75
minako-h <hawaii77777m@icloud.com>
2020-05-05 20:31:49 +0900

    fix form

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   root to: 'tops#index'
   resources :menus, only: [:index]
   resources :movies, only: [:index] do
-    resources :comments, only: [:create]
+    resources :bese, only: [:index]
   end
+  # resources :movies, only: [:index] do
+  #   resources :comments, only: [:create]
+  # end
+  resources :comments, only: [:create]
   resources :contacts, only: [:new, :create]
   resources :users, only: [:show]
   resources :posts, only: [:index, :new, :create, :show]
