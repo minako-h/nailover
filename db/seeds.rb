7f2f965decc7a0309b4229c7d632e1aa772c34d4
minako-h <hawaii77777m@icloud.com>
2020-05-10 16:06:39 +0900

    seed create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+Movie.create!(
+  [
+    {
+      category: 'base'
+      src: 'https://www.youtube.com/embed/h-5mNdTobDA'
+    },
+    {
+      category: 'base'
+      src: 'https://www.youtube.com/embed/XIEF0rGoc4Y'
+    },
+    {
+      category: 'color'
+      src: 'https://www.youtube.com/embed/mR8dLTSjDFY'
+    },
+    {
+      category: 'color'
+      src: 'https://www.youtube.com/embed/xQZ_79fldow'
+    },
+    {
+      category: 'top'
+      src: 'https://www.youtube.com/embed/CdF_6meHYRQ'
+    },
+    {
+      category: 'top'
+      src: 'https://www.youtube.com/embed/B8_bJBrGGEU'
+    },
+    {
+      category: 'art'
+      src: 'https://www.youtube.com/embed/HEoilr96HOA'
+    },
+    {
+      category: 'art'
+      src: 'https://www.youtube.com/embed/_o6C72_jsCg'
+    }
+    {
+      category: 'polish'
+      src: 'https://www.youtube.com/embed/P-YZTFYi5zQ'
+    },
+    {
+      category: 'polish'
+      src: 'https://www.youtube.com/embed/cym2ejZmkYo'
+    }
+  ]
+)
